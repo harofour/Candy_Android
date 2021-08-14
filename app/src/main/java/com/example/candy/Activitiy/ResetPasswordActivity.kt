297ef6a5e7ce72df710ea74b648753cc34222355
김상훈 <uasang01@name.com>
2021-08-14 14:32:58 +0900

    로그인 후 User 정보, jwt 토큰 저장베이스 엑티비티, 비밀번호 재설정 엑티비티 추가베이스 엑티비티, 비밀번호 재설정 엑티비티 추가

--- app/src/main/java/com/example/candy/Activitiy/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/candy/Activitiy/ResetPasswordActivity.kt
@@ -2,16 +2,13 @@ package com.example.candy.Activitiy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import com.example.candy.R
-import com.example.candy.databinding.ActivityFindPasswordBinding
-import com.example.candy.databinding.ActivityLogInBinding
-import com.example.candy.utils.Util
+import com.example.candy.databinding.ActivityResetPasswordBinding
 
-class FindPasswordActivity : AppCompatActivity() {
-    val Tag = "FindPasswordActivity"
-    private var mBinding: ActivityFindPasswordBinding? = null
+class ResetPasswordActivity : BaseActivity() {
+    val Tag = "ResetPasswordActivity"
+    private var mBinding: ActivityResetPasswordBinding? = null
     private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +20,10 @@ class FindPasswordActivity : AppCompatActivity() {
             setDisplayShowCustomEnabled(true)
             setDisplayShowTitleEnabled(true)
             setDisplayHomeAsUpEnabled(true)
-            title = "비밀번호 찾기"
+            title = "비밀번호 재설정"
         }
 
-        mBinding = ActivityFindPasswordBinding.inflate(layoutInflater)
+        mBinding = ActivityResetPasswordBinding.inflate(layoutInflater)
 
         initListeners()
 
@@ -34,55 +31,7 @@ class FindPasswordActivity : AppCompatActivity() {
 
     fun initListeners(){
         with(binding){
-            sendEmailAuthBtn.setOnClickListener {
-//                sendEmailAuthBtn.isClickable = false
-                val times = 5
-                Util.timer(
-                    times = times,
-                    timeMillis = 1000,
-                    action = {
-                        Log.d(Tag, "action / ${timeToString(times - it)} / ${sendEmailAuthBtn.isClickable}")
-                        runOnUiThread{
-                            sendEmailAuthBtn.alpha = 0.5f
-                        }
-                    },
-                    finished= {
-                        sendEmailAuthBtn.isClickable = true
 
-                        runOnUiThread{
-                            sendEmailAuthBtn.alpha = 1f
-                        }
-                    }
-                )
-            }
         }
-
-    }
-    private fun timeToString(time: Int) : String {
-        var min = ""
-        var sec = ""
-        if(time>120){
-            min = "2"
-            sec = "${time-120}"
-        }else if(time>60){
-            min = "1"
-            sec = "${time-60}"
-        }else{
-            min = "0"
-            sec = "$time"
-        }
-        return "$min : $sec"
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                //toolbar의 back키 눌렀을 때 동작
-                finish()
-                return true
-            }
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 }
