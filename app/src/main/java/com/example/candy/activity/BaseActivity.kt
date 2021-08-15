220d0282f3594b8282a808fc665874e710033760
김상훈 <uasang01@name.com>
2021-08-16 02:29:30 +0900

    코드 정리

--- app/src/main/java/com/example/candy/activity/BaseActivity.kt
+++ app/src/main/java/com/example/candy/activity/BaseActivity.kt
@@ -8,7 +8,6 @@ import com.example.candy.R
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
